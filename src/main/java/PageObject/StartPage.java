fdb4fcf988459841d5b79bb40476c979131c1830
Dmitriy Suntsov <theonewhoplays1337@gmail.com>
2023-01-20 23:28:20 +0300

    added reports

--- src/main/java/PageObject/StartPage.java
+++ src/main/java/PageObject/StartPage.java
@@ -6,9 +6,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class StartPage {
     private WebDriver driver;
+
     public StartPage(WebDriver driver) {
         this.driver = driver;
     }
+
     private By personalAccount = By.xpath(".//a[@class = 'AppHeader_header__link__3D_hX' and @href = '/account']");
     private By mainPageLoginButton = By.xpath("//*[@id=\"root\"]/div/main/section[2]/div/button");
     private By bunsButton = By.xpath("//*[@class='text text_type_main-default' and text() = 'Булки']");
@@ -17,73 +19,79 @@ public class StartPage {
     private By expectedBun = By.xpath("//*[@class='BurgerIngredient_ingredient__text__yp3dH' and text() = 'Краторная булка N-200i']");
     private By expectedSauce = By.xpath("//*[@class='BurgerIngredient_ingredient__text__yp3dH' and text() = 'Соус с шипами Антарианского плоскоходца']");
     private By expectedFilling = By.xpath("//*[@class='BurgerIngredient_ingredient__text__yp3dH' and text() = 'Говяжий метеорит (отбивная)']");
-    public void clickMainPageLoginButton(){
+
+    public void clickMainPageLoginButton() {
         driver.findElement(mainPageLoginButton).click();
     }
-    public void clickOnPersonalAccount(){
+
+    public void clickOnPersonalAccount() {
         driver.findElement(personalAccount).click();
     }
-    public void clickOnBunsButton(){
-        driver.findElement(bunsButton).click();
-    }
-    public void clickOnSauceButton(){
+
+    public void clickOnSauceButton() {
         driver.findElement(sauceButton).click();
     }
-    public void clickOnFillingsButton(){
+
+    public void clickOnFillingsButton() {
         driver.findElement(fillingsButton).click();
     }
-    public String gettingTheCurrentURL(){
+
+    public String gettingTheCurrentURL() {
         String currentURL = driver.getCurrentUrl();
         return currentURL;
     }
 
-    public void waitForPersonalAccountToLoad(){
-       new WebDriverWait(driver, 10).until(driver1 -> ((driver.findElement(personalAccount).isDisplayed())));
+    public void waitForPersonalAccountToLoad() {
+        new WebDriverWait(driver, 10).until(driver1 -> ((driver.findElement(personalAccount).isDisplayed())));
     }
-    public void waitForBunsButtonToLoad(){
+
+    public void waitForBunsButtonToLoad() {
         new WebDriverWait(driver, 10).until(driver1 -> ((driver.findElement(bunsButton).isDisplayed())));
     }
-    public void waitForBunsToLoad(){
+
+    public void waitForBunsToLoad() {
         new WebDriverWait(driver, 10).until(driver1 -> ((driver.findElement(expectedBun).isDisplayed())));
     }
-    public void waitForSaucesToLoad(){
+
+    public void waitForSaucesToLoad() {
         new WebDriverWait(driver, 10).until(driver1 -> ((driver.findElement(expectedSauce).isDisplayed())));
     }
-    public void waitForFillingsToLoad(){
+
+    public void waitForFillingsToLoad() {
         new WebDriverWait(driver, 10).until(driver1 -> ((driver.findElement(expectedFilling).isDisplayed())));
     }
 
-    public boolean checkIfBunIsVisible(){
+    public boolean checkIfBunIsVisible() {
         boolean resultBuns = driver.findElement(expectedBun).isDisplayed();
         return resultBuns;
     }
-    public boolean checkIfSauceIsVisible(){
+
+    public boolean checkIfSauceIsVisible() {
         boolean resultSauces = driver.findElement(expectedSauce).isDisplayed();
         return resultSauces;
     }
-    public boolean checkIfFillingIsVisible(){
+
+    public boolean checkIfFillingIsVisible() {
         boolean resultFillings = driver.findElement(expectedFilling).isDisplayed();
         return resultFillings;
     }
 
-    public boolean clickAndWaitAndCheckVisibilityBuns(){
+    public boolean clickAndWaitAndCheckVisibilityBuns() {
         waitForBunsButtonToLoad();
-        //clickOnBunsButton();
         waitForBunsToLoad();
         return checkIfBunIsVisible();
     }
-    public boolean clickAndWaitAndCheckVisibilitySauces(){
+
+    public boolean clickAndWaitAndCheckVisibilitySauces() {
         clickOnSauceButton();
         waitForSaucesToLoad();
         return checkIfSauceIsVisible();
     }
-    public boolean clickAndWaitAndCheckVisibilityFillings(){
+
+    public boolean clickAndWaitAndCheckVisibilityFillings() {
         clickOnFillingsButton();
         waitForFillingsToLoad();
         return checkIfFillingIsVisible();
     }
-
-
-
 
 }
